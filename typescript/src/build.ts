e20163742661a0bcb0d7dd5b2da27e74c002e316
David Roussov <david.roussov@gmail.com>
2024-03-07 07:39:02 +1100

    generate typescript types

--- typescript/src/build.ts
+++ typescript/src/build.ts
@@ -1,1 +1,9 @@
-console.log('test');
+import * as fs from 'fs';
+import { compileFromFile } from 'json-schema-to-typescript';
+
+(async () => {
+  const schemaPath = 'src/schemas/curated-listing.json';
+  const modelsPath = 'src/models/models.d.ts';
+  const ts = await compileFromFile(schemaPath);
+  fs.writeFileSync(modelsPath, ts);
+})();
