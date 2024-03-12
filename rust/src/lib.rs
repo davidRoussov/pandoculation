c7c1b49d3571bdebd943372d31653da82a9298c6
David Roussov <david.roussov@gmail.com>
2024-03-13 07:42:22 +1100

    fix rust lib

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,22 +1,25 @@
- #[derive(Deserialize, Serialize, Debug, Clone)]
- pub struct CuratedListing {
-     items: Vec<CuratedListingItem>,
- }
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
- #[derive(Deserialize, Serialize, Debug, Clone)]
- pub struct CuratedListingItem {
-     data: CuratedListingItemData,
- }
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CuratedListing {
+    items: Vec<CuratedListingItem>,
+}
 
- #[derive(Deserialize, Serialize, Debug, Clone)]
- pub struct CuratedListingItemData {
-     title: String,
-     url: String,
-     author: Option<String>,
-     id: Option<String>,
-     points: Option<String>,
-     timestamp: Option<String>,
-     chatLink: Option<String>,
-     #[serde(flatten)]
-     additional: HashMap<String, String>,
- }
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CuratedListingItem {
+    data: CuratedListingItemData,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CuratedListingItemData {
+    title: String,
+    url: String,
+    author: Option<String>,
+    id: Option<String>,
+    points: Option<String>,
+    timestamp: Option<String>,
+    chat_url: Option<String>,
+    #[serde(flatten)]
+    additional: HashMap<String, String>,
+}
